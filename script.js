f61d0f937fb20fc35cb423a2b16f6ad5feb22043
christina-aguiar <123034210+christina-aguiar@users.noreply.github.com>
2023-06-01 07:03:35 +0200

    create grid pattern

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const container = document.querySelector('.container');
-let numOfGrids;
-function gridMaker(numOfGrids) {
-    for (i = 0; i < numOfGrids; i++) {
-        const grid = document.createElement(`div`);
-        grid.classList.add("grid");
-        grid.textContent = "";
-        container.appendChild(grid);
+
+let block;
+let container;
+
+function makeRows(numOfBlocks) {
+    for (i = 0; i < numOfBlocks; i++) {
+        const container = document.createElement('div');
+        container.classList.add("container");
+        document.body.appendChild(container);
+    for (j = 0; j < numOfBlocks; j++) {
+        const block = document.createElement(`div`);
+        block.classList.add("block");
+        block.textContent = "";
+        container.appendChild(block);
     }
+}
 }
