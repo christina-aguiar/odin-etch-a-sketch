653f9d9b223f8f5657b53bd3c7a7ed0ac026c19d
christina-aguiar <123034210+christina-aguiar@users.noreply.github.com>
2023-06-02 21:28:39 +0200

    rename functions

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 const remake = document.querySelector('.remake');
 const inputRows = document.querySelector('input');
 const reset = document.querySelector('.reset');
-let numOfBlocks = inputRows.value; 
+const container = document.querySelector('.container')
+let numOfBlocks = inputRows.value;
 
-reset.addEventListener('click', function() {
+reset.addEventListener('click', function () {
     deleteAll();
     inputRows.value = 0;
     makeRows(inputRows.value);
 });
 
-remake.addEventListener('click', function() {
+remake.addEventListener('click', function () {
     deleteAll();
     makeRows(inputRows.value);
 });
 
 function makeRows(numOfBlocks) {
     for (i = 0; i < numOfBlocks; i++) {
-        const container = document.createElement('div');
-        container.classList.add("container");
-        document.body.appendChild(container);
-    for (j = 0; j < numOfBlocks; j++) {
-        const block = document.createElement(`div`);
-        block.classList.add("block");
-        block.textContent = "";
-        block.addEventListener("mouseenter", function() {
-            block.classList.add('active')
-        })
-        container.appendChild(block);
+        const row = document.createElement('div');
+        row.classList.add("row");
+        container.appendChild(row);
+        for (j = 0; j < numOfBlocks; j++) {
+            const block = document.createElement(`div`);
+            block.classList.add("block");
+            block.textContent = "";
+            block.addEventListener("mouseenter", function () {
+                block.classList.add('active')
+            })
+            row.appendChild(block);
+        }
     }
-}
 };
 
 function deleteAll() {
-    const containers = document.querySelectorAll('.container');
-    containers.forEach(Element => Element.remove());
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(Element => Element.remove());
 };
 
 
